c70a7b6163c09ef21f4f83264f8c73639f9d5147
Samuel Osho <samthatcode@gmail.com>
2023-10-25 14:31:33 +0100

    bug fix

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var cors = require('cors');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' }); // Specifies the destination for uploaded files
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+// Endpoint to handle file uploads
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  // req.file is the `upfile` file
+  // req.body will hold the text fields, if there were any
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+})
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
